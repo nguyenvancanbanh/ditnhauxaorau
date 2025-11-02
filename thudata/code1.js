1249300e1a6fc495536631f930ae9fdb74361bed
Nguyen Van Can <nguyenvancanbanh@gmail.com>
2025-11-02 20:28:34 +0700

    first commit

---
+++ thudata/code1.js
@@ -0,0 +1,1 @@
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyFjkDQu3slIHi_qm1FAAM1NtxbQ-DAk2jghoWX6mYD4bw_voDhtVo788SVGlry9Xwi/exec";
---
+++ thudata/code2.js
@@ -0,0 +1,1 @@
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzeQebCsk89OXjzxvGEMG-0DNqm-cQ2Pyx36oVmJQ4wwD5OnuLmKp3GuO97dEjKweGK/exec";
